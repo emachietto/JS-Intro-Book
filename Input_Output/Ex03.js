4bffa559000f4c0022e268f8639a9f7da5aed60c
Elaine Machietto <emachietto@gmail.com>
2024-06-14 20:22:47 -0400

    Added Input/Output chapter exercises

---
+++ Input_Output/Ex03.js
@@ -0,0 +1,12 @@
+let age = Number(require('readline-sync').question('How old are you?\n'));
+// wanted to do it w/out a var for fun/practice
+
+function calculateFutureAge(age) {
+  console.log(`You are ${age} years old.`);
+  console.log(`In 10 years, you will be ${age + 10} years old.`);
+  console.log(`In 20 years, you will be ${age + 20} years old.`);
+  console.log(`In 30 years, you will be ${age + 30} years old.`);
+  console.log(`In 40 years, you will be ${age + 40} years. old`);
+};
+
+calculateFutureAge(age);
